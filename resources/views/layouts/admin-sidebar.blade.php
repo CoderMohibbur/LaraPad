3f4791ee31e5ad00c404a431751776796d722e5d
user <mdtufsirulmiraj@gmail.com>
2025-07-30 09:51:45 +0600

    update

--- resources/views/layouts/admin-sidebar.blade.php
+++ resources/views/layouts/admin-sidebar.blade.php
@@ -145,7 +145,7 @@
 
             {{-- Setting --}}
             <li>
-                <a href="#"
+                <a href="admin/logo"
                     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
 
                     <i data-lucide="settings"></i>
